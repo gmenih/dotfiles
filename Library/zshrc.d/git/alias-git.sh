8c8c4973290eaa2335e3d424ae860dfaa61edbac
Gregor Menih <gregor@menih.si>
2021-08-12 09:38:29 +0200

    update

--- Library/zshrc.d/git/alias-git.sh
+++ Library/zshrc.d/git/alias-git.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
 alias gpf='ggsup && gp -f'
-alias grom="git reset --hard origin/master"
-alias grpo="git remote prune origin"
+alias grom='git reset --hard origin/master'
+alias grpo='git remote prune origin'
+alias gs='git status'
+alias gll='git pull'
---
+++ Library/zshrc.d/git/gml.sh
@@ -0,0 +1,4 @@
+function gml {
+	git checkout master
+	git pull
+}
