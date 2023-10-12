1f10e248d26143f2b57be3fae17c9945cedcd0bb
Gregor Menih <gregor@menih.si>
2023-10-12 10:12:10 +0200

    updates

--- home/private_dot_config/nvim/lua/gmenih/remap.lua
+++ home/private_dot_config/nvim/lua/gmenih/remap.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+function ca_db()
+	vim.api.nvim_feedkeys('da{', 'n', false)
+	vim.api.nvim_feedkeys('ca{', 'n', false)
+end
+
+vim.keymap.set('n', '<leader>ca{', ca_db)
+
