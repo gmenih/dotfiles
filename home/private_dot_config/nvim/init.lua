8b016a05d8135b0621a525ccf387b55c89a30439
Gregor Menih <gregor@menih.si>
2023-10-13 12:44:43 +0200

    updates

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,2 +1,4 @@
-require("gmenih.core")
-require("gmenih.lazy")
+if not vim.g.vscode then
+	require("gmenih.core")
+	require("gmenih.lazy")
+end
--- home/private_dot_config/nvim/lua/gmenih/core/options.lua
+++ home/private_dot_config/nvim/lua/gmenih/core/options.lua
@@ -5,7 +5,7 @@ opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 4 -- 4 spaces for tabs 
+opt.tabstop = 4 -- 4 spaces for tabs
 opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
@@ -32,7 +32,7 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
--- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
