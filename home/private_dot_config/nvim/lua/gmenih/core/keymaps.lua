177bcd31ca0ee2d83afbdd93b8dfb7ce2350dc3a
Gregor Menih <gregor@menih.si>
2023-10-25 10:54:05 +0200

    Add neogit plugin

--- home/private_dot_config/nvim/lua/gmenih/core/keymaps.lua
+++ home/private_dot_config/nvim/lua/gmenih/core/keymaps.lua
@@ -8,9 +8,10 @@ local keymap = vim.keymap -- for conciseness
 
 keymap.set("i", "<esc><esc>", "<esc>:w<CR>", { desc = "Save file and exit insert mode" })
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
 -- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x')
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
