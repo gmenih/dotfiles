24f5824bb41c05c65d65621135a193870bb32aa8
Gregor Menih <gregor@menih.si>
2023-12-10 20:20:21 +0100

    updates

--- home/private_dot_config/nvim/lua/gmenih/plugins/comment.lua
+++ home/private_dot_config/nvim/lua/gmenih/plugins/comment.lua
@@ -1,24 +1,7 @@
--- Quickly add comments to text
--- KeyBindings:
--- gc -- line comment
--- gb -- block comment
--- +o/O -- add it to upper/lower line
 return {
     "numToStr/Comment.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+        -- add any options here
     },
-    config = function()
-        -- import comment plugin safely
-        local comment = require("Comment")
-
-        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-        -- enable comment
-        comment.setup({
-            -- for commenting tsx and jsx files
-            pre_hook = ts_context_commentstring.create_pre_hook(),
-        })
-    end,
+    lazy = false,
 }
